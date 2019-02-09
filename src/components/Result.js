1cc9046b11837a45c9034c3c5909e7deb3af06bb
Frank.js <46870205+FilipFranek1@users.noreply.github.com>
2019-02-09 15:51:13 +0100

    Update Result.js

--- src/components/Result.js
+++ src/components/Result.js
@@ -7,9 +7,7 @@ const Result = props => {
             <img src={img} alt="" />
             <br />
             <div className="text">
-                <h2>Tarnów:</h2>
-                <p>Longtidue: 20.986601<br />
-                    Latidue: 50.015732 </p>
+         
                 <a href="https://www.latlong.net/" target="_blank" rel="noopener noreferrer">more coordinates</a>
             </div>
         </div>
